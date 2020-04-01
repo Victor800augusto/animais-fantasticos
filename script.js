9ff1143037be488955810584dd88e12b4a8e8155
Victor Souza <victor.augusto800@gmail.com>
2020-04-01 15:58:45 -0300

    Animação no scroll

--- script.js
+++ script.js
@@ -68,4 +68,28 @@ function initScrollSuave() {
         link.addEventListener('click', scrollToSection);
     });
 }
+
 initScrollSuave();
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+                console.log(sectionTop);
+                if (sectionTop < 0) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+        animaScroll();
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+
+initAnimacaoScroll();
